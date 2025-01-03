911a4b53357bac1599d1e3bf079d6a6fc97c7c28
semajame <jamesanquillano@gmail.com>
2025-01-03 19:05:34 +0800

    First commit

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,44 +1,46 @@
-<script setup lang="ts">
+<script setup>
 import { DASHBOARD_CARDS } from '~/consts/cards'
 
 definePageMeta({ middleware: 'auth', auth: { guestRedirectUrl: '/sign-in' } })
 </script>
 
 <template>
-  <main class="max-w-[75rem] w-full mx-auto">
-    <div class="grid grid-cols-[1fr_20.5rem] gap-10 pb-10">
-      <div>
-        <header class="flex items-center justify-between w-full h-16 gap-4">
-          <div class="flex gap-4">
-            <ClerkLogo />
-            <div aria-hidden class="w-px h-6 bg-[#C7C7C8]" />
-            <NuxtLogo />
-          </div>
-          <div class="flex items-center gap-2">
-            <OrganizationSwitcher
-              :appearance="{
-                elements: {
-                  organizationPreviewAvatarBox: 'size-6',
-                },
-              }"
-            />
-            <UserButton
-              :appearance="{
-                elements: {
-                  userButtonAvatarBox: 'size-6',
-                },
-              }"
-              default-open
-            />
-          </div>
-        </header>
-        <UserDetails />
+  <div>
+    <main class="container mx-auto">
+      <div class="pb-10">
+        <div>
+          <header class="flex items-center justify-between w-full h-16 gap-4">
+            <div class="flex gap-4">
+              <ClerkLogo />
+              <div aria-hidden class="w-px h-6 bg-[#C7C7C8]" />
+              <NuxtLogo />
+            </div>
+            <div class="flex items-center gap-2">
+              <!-- <OrganizationSwitcher
+                :appearance="{
+                  elements: {
+                    organizationPreviewAvatarBox: 'size-6',
+                  },
+                }"
+              /> -->
+              <UserButton
+                :appearance="{
+                  elements: {
+                    userButtonAvatarBox: 'size-6',
+                  },
+                }"
+                default-open
+              />
+            </div>
+          </header>
+          <UserDetails />
+        </div>
+        <!-- <div class="pt-[3.5rem]">
+          <CodeSwitcher />
+        </div> -->
       </div>
-      <div class="pt-[3.5rem]">
-        <CodeSwitcher />
-      </div>
-    </div>
-  </main>
-  <LearnMore :cards="DASHBOARD_CARDS" />
-  <Footer />
+    </main>
+    <LearnMore :cards="DASHBOARD_CARDS" />
+    <Footer />
+  </div>
 </template>
